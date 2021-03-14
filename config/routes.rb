87f6548fcabf6b3523be0264d6ab5c6c0806948f
sk1318 <sk13188559@outlook.jp>
2021-03-14 05:25:17 +0000

    first_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
- root to: "books#home"
-  post 'books' => 'books#create',as: 'create_book'
-  get 'books' => 'books#index',as: 'books'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
+ root to: "homes#top"
+ devise_for :users
+ resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
